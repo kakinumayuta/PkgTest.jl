d513c78daaefd530b3a336f6d5700224b0dd2e63
Documenter.jl <documenter@juliadocs.github.io>
2025-06-20 06:59:07 +0000

    build based on a50f3d4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PkgTest","page":"Home","title":"PkgTest","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PkgTest.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
